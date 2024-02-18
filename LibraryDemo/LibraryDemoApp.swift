580507b13e896e27432b3d847e1f5dddcadbab1c
vixer93 <sasasa.24.blue@gmail.com>
2024-02-18 22:19:21 +0900

    First

---
+++ LibraryDemo/LibraryDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryDemoApp.swift
+//  LibraryDemo
+//
+//  Created by 薄田晋 on 2024/02/18.
+//
+
+import SwiftUI
+
+@main
+struct LibraryDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
